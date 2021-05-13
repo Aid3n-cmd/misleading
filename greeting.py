0f5a54b2585b3f33069b60639c24f094bc451362
ThatGuyAid3n <83622490+Aid3n-cmd@users.noreply.github.com>
2021-05-13 14:13:04 -0400

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,15 +1,56 @@
-# the variable g stands for "greeting" dont mind the p
-g = "good morning, you are retarded"
+import time
+print('Misleading INC. Tech Department.')
+userInput = input('Enter the api password.\n')
 
+#stuff for regular input for api password
+if userInput == 'Let Me In':
+    hi = input ('Hello! \n')
+    print ("I am A.I.D.E.N.")
+    name = input('whats your name? \n')
+    if name == 'Aiden':
+            print('Hello Co-Owner of Misleading INC.')
+    if name == 'Wyatt':
+        print('Hello Head Technical Developer at Misleading INC.')
+    time.sleep(1)    
+    print ('Started Misleading API')
+    time.sleep(.9)
+    print('starting bot...')
+    time.sleep(4)
+    print('load 4%')
+    time.sleep(1)
+    print('load 13%')
+    time.sleep(1)
+    print('load 34%')
+    time.sleep(1)
+    print('load 48%')
+    time.sleep(.7)
+    print('load 56%')
+    time.sleep(.7)
+    print('load 69% (nice)')
+    time.sleep(1)
+    print('load 84%')
+    time.sleep(.6)
+    print('load 95%')
+    time.sleep(2)
+    print('load 100%')
+    time.sleep(3)
+    print('started!')
+    
+    print('Hi', name, '')
+    mood = input('how are you?')
+    print ('I am also ', mood, '')
+    time.sleep(3)
+    end = input('awaiting command input...\n')
+    
 
+#devtest things for testing maybe        
+if userInput == 'devtest':
+    print('devtest initiated')
+    
+    
+#commands for the last variable in regular input (variable end)
+if end == 'help':
+        print('This command will work when enough commands are implemented.')
 
-
-# dont mess with this
-
-print g
-d = today.strftime("%m/%d/%y")
-print("todays date is")
-print(d3) 
-print(p)
-
-
+if end == 'stop':
+    print('end of program (you can close this window)\n')
