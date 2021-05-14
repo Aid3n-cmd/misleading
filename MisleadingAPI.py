d917704dc9eab14a634e8dd1fd0f642a25b59123
ThatGuyAid3n <83622490+Aid3n-cmd@users.noreply.github.com>
2021-05-14 08:40:50 -0400

    Update MisleadingAPI.py

--- MisleadingAPI.py
+++ MisleadingAPI.py
@@ -1,6 +1,6 @@
 import time
 print('Misleading INC. Tech Department.')
-userInput = input('Enter the api password.\n')
+userInput = input('Enter the api assword.\n')
 
 #stuff for regular input for api password
 #the correct password
@@ -48,13 +48,6 @@ if userInput == 'Shantry':
     print('Hi', name, '')
     time.sleep(3)
     end = input('awaiting cum and input...\n')
-    
-    #commands for the last variable in regular input (variable end)
-if end == 'help':
-        print('This command will work when enough commands are implemented.')
-
-if end == 'stop':
-    print('end of program (you can close this window)\n')
 
    
 if userInput == 'Blast.Some.Ass.':
@@ -63,11 +56,60 @@ if userInput == 'Blast.Some.Ass.':
     print('Gucci Flip Flops fully operational.')
     print('State of the art ass kickers fully operational.')
     print('Error.')
-    print('Please input the Asskicking Assword.')
-if userInput == 'THROW THAT BACK LIKE A CADILAC':
+    ass = input('Please input the Asskicking Assword.\n')
+    
+
+if ass == 'THROW THAT BACK LIKE A CADILAC':
     print('Throwing school busses off of bridges...')
     print('Laying mines inside of schoolyards...')
     print('Eating orphans...')
     print('Collecting ophan tears.')
-    print('All done! you may now commit all the attrosities you want! Have fun!')
+    print('Correct\n')
+    print ("===Misleading API=== \n")
+    
+    
+    name = input('Name:\n')
+    if name == 'Aiden':
+            print('Hello Co-Owner of Misleading INC.\n')
+    if name == 'Kaleb':
+        print('Hello Owner of Misleading INC.')
+    if name == 'Asshole':
+        print('Hello knvoefhboienvefv.')
+    if name == 'ijgbniuwgnvignvgubnsibngrbnsjbhsiuwtnvdfbeuvnfvnefuvwhskodjewifnve':
+        print('fuckshitbitchdamncocksuckerpussyassholecuntmotherfuckindirtywhoershatontomylunchfuckshitdamnbitchsuckmydickjesusharoldchristshitbitchcocksuckergoddamnmotherfuckerpussyassholecunt')
+    if name == '':
+              print('so you dont have a name???')
+    time.sleep(1)    
+    print ('Started Misleading API')
+    time.sleep(.9)
+    print('starting thot...')
+    time.sleep(4)
+    print('load 4%')
+    time.sleep(1)
+    print('load 13%')
+    time.sleep(1)
+    print('load 34%')
+    time.sleep(1)
+    print('load 48%')
+    time.sleep(.7)
+    print('load 56%')
+    time.sleep(.7)
+    print('load 69% (nice)')
+    time.sleep(1)
+    print('load 84%')
+    time.sleep(.6)
+    print('load 95%')
+    time.sleep(2)
+    print('load 100%')
+    time.sleep(3)
+    print('sharted!')
+    
+    print('Hi', name, '')
+    time.sleep(3)
+    end = input('awaiting cum and input...\n')
+    
+    asst = input('All done! you may now commit all the attrosities you want! Have fun!')
+    
+if asst == '':
+    Input('Hey theres no words there?!?!?!')   
    
