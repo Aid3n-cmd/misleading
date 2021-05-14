d8a492e14613d738c21992318dddb0e4d7a331d9
ThatGuyAid3n <83622490+Aid3n-cmd@users.noreply.github.com>
2021-05-14 09:28:02 -0400

    Update MisleadingAPI.py

--- MisleadingAPI.py
+++ MisleadingAPI.py
@@ -7,49 +7,14 @@ userInput = input('Enter the api assword.\n')
 if userInput == 'Shantry':
     print('Correct\n')
     print ("===Misleading API=== \n")
+    input('that is one of the correct passwords but not the coolest one...')
+    nameone = input('name:\n')
+    if nameone == 'Aiden'
+        print('Hello Co-Owner of Misleading INC')
+cmdline1 = input('awaiting command input')
+if cmdline1 == help
+    input('well... we havent really made any commands yet but there will be some soon')
     
-    
-    name = input('Name:\n')
-    if name == 'Aiden':
-            print('Hello Co-Owner of Misleading INC.\n')
-    if name == 'Kaleb':
-        print('Hello Owner of Misleading INC.')
-    if name == 'Asshole':
-        print('Hello knvoefhboienvefv.')
-    if name == 'ijgbniuwgnvignvgubnsibngrbnsjbhsiuwtnvdfbeuvnfvnefuvwhskodjewifnve':
-        print('fuckshitbitchdamncocksuckerpussyassholecuntmotherfuckindirtywhoershatontomylunchfuckshitdamnbitchsuckmydickjesusharoldchristshitbitchcocksuckergoddamnmotherfuckerpussyassholecunt')
-    if name == '':
-              print('so you dont have a name???')
-    time.sleep(1)    
-    print ('Started Misleading API')
-    time.sleep(.9)
-    print('starting thot...')
-    time.sleep(4)
-    print('load 4%')
-    time.sleep(1)
-    print('load 13%')
-    time.sleep(1)
-    print('load 34%')
-    time.sleep(1)
-    print('load 48%')
-    time.sleep(.7)
-    print('load 56%')
-    time.sleep(.7)
-    print('load 69% (nice)')
-    time.sleep(1)
-    print('load 84%')
-    time.sleep(.6)
-    print('load 95%')
-    time.sleep(2)
-    print('load 100%')
-    time.sleep(3)
-    print('sharted!')
-    
-    print('Hi', name, '')
-    time.sleep(3)
-    end = input('awaiting cum and input...\n')
-
-   
 if userInput == 'Blast.Some.Ass.':
     print('Assblasters initiated.')
     print('Running full system diagnostics')
