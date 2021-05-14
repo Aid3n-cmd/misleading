ade34260c5dadcafb8f1ca8c1dcf5daf8cbc025a
YourPapaWhiskey <83650360+YourPapaWhiskey@users.noreply.github.com>
2021-05-14 09:10:27 -0400

    Update MisleadingAPI.py

--- MisleadingAPI.py
+++ MisleadingAPI.py
@@ -56,7 +56,7 @@ if userInput == 'Blast.Some.Ass.':
     print('Gucci Flip Flops fully operational.')
     print('State of the art ass kickers fully operational.')
     print('Error.')
-    ass = userInput('Please input the Asskicking Assword.\n')
+    ass = Input('Please input the Asskicking Assword.\n')
     
 
 if ass == 'THROW THAT BACK LIKE A CADILAC':
